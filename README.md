a17c8dc1eac9ff4d1d3e1d9c94352070d4bca69f
Ila Muniz <ilammuniz@gmail.com>
2022-10-26 11:29:04 -0300

    Seventh commit: finalizing challenge

--- README.md
+++ README.md
@@ -1,3 +1,17 @@
-# Desafio: Abstraindo um Bootcamp usando Orientação a Objetos em Java
-## Atividade do Bootcamp Potência Tech Powered by IFood - Java Beginners
-### O objetivo principal do desafio é colocar em prática as principais ferramentas da OO: Abstração, Encapsulamento, Herança e Polimorfismo em um projeto Java
+## Desafio: Abstraindo um Bootcamp usando Orientação a Objetos em Java
+
+### Atividade do Bootcamp Potência Tech Powered by IFood - Java Beginners
+
+O objetivo principal do desafio é colocar em prática as principais ferramentas da OO: Abstração, Encapsulamento, Herança e Polimorfismo em um projeto Java
+
+### Etapas
+
+    1. Abstrair o domínio bootcamp e modelar os atributos e métodos
+    2. Criar as classes Bootcamp, Curso, Mentoria e Dev
+    3. Criar os atributos e métodos das classes Curso, Mentoria e Dev
+    4. Usar as ferramentas de Herança, Abstração, Encapsulamento e Polimorfismo
+    5. Transformar as classes em objetos
+
+### Repositório de referência
+
+[Repositório desafio-poo-dio](https://github.com/cami-la/desafio-poo-dio) por [Camila Cavalcante](https://github.com/cami-la)
--- src/Main.java
+++ src/Main.java
@@ -1,4 +1,6 @@
+import br.com.dio.desafio.dominio.Bootcamp;
 import br.com.dio.desafio.dominio.Curso;
+import br.com.dio.desafio.dominio.Dev;
 import br.com.dio.desafio.dominio.Mentoria;
 
 import java.time.LocalDate;
@@ -21,8 +23,40 @@ public class Main {
         mentoria.setDescricao("Descrição da mentoria de Java");
         mentoria.setData(LocalDate.now());
 
-        System.out.println(curso1);
+        /*System.out.println(curso1);
         System.out.println(curso2);
-        System.out.println(mentoria);
+        System.out.println(mentoria);*/
+
+        Bootcamp bootcamp = new Bootcamp();
+        bootcamp.setNome("Bootcamp Java Developer");
+        bootcamp.setDescricao("Descrição do Bootcamp Java Developer");
+        bootcamp.getConteudos().add(curso1);
+        bootcamp.getConteudos().add(curso2);
+        bootcamp.getConteudos().add(mentoria);
+
+        Dev devIla = new Dev();
+        devIla.setNome("Ila Muniz");
+        devIla.inscreverBootcamp(bootcamp);
+        System.out.println("Conteúdos inscritos de Ila Muniz: " + devIla.getConteudosInscritos());
+        devIla.progredir();
+        devIla.progredir();
+        System.out.println("---");
+        System.out.println("Conteúdos inscritos de Ila Muniz: " + devIla.getConteudosInscritos());
+        System.out.println("Conteúdos concluídos de Ila Muniz: " + devIla.getConteudosConcluidos());
+        System.out.println("XP: " + devIla.calcularTotalXp());
+
+        System.out.println("-------------------------------");
+
+        Dev devMiguel = new Dev();
+        devMiguel.setNome("Miguel");
+        devMiguel.inscreverBootcamp(bootcamp);
+        System.out.println("Conteúdos inscritos de Miguel: " + devMiguel.getConteudosInscritos());
+        devMiguel.progredir();
+        devMiguel.progredir();
+        devMiguel.progredir();
+        System.out.println("---");
+        System.out.println("Conteúdos inscritos de Miguel: " + devMiguel.getConteudosInscritos());
+        System.out.println("Conteúdos concluídos de Miguel: " + devMiguel.getConteudosConcluidos());
+        System.out.println("XP: " + devMiguel.calcularTotalXp());
     }
 }
